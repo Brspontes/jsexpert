e9290ed7b00afd02334beb71a89264da34bf10ce
Brian Pontes <brian.robert16@hotmail.com>
2022-05-30 20:55:10 -0300

    trabalhando com testes - aula 1

---
+++ javascript-testing/aula-01/src/file.js
@@ -0,0 +1,47 @@
+const { readFile } = require('fs/promises')
+const { join } = require('path')
+const { error } = require('./constants')
+const DEFAULT_OPTION = {
+  maxLines: 3,
+  fields: ['id', 'name', 'profession', 'age']
+}
+class File {
+  static async csvToJson(filepath) {
+    const content = await File.getFileContent(filepath)
+    const validation = File.isValid(content)
+    if(!validation.valid) throw new Error(validation.error)
+    return content
+  }
+
+  static async getFileContent(filepath) {
+    const filename = join(__dirname, filepath)
+    return (await readFile(filename)).toString('utf8')
+  }
+
+  static isValid(csvString, options = DEFAULT_OPTION) {
+    const [header, ...fileWithoutHeader] = csvString.split('\n')
+    const isHeaderValid = header === options.fields.join(',')
+    if(!isHeaderValid) {
+      return {
+        error: error.FILE_FIELDS_ERROR_MESSAGE,
+        valid: false
+      }
+    }
+    const isContentLengthAccepted = (
+      fileWithoutHeader.length > 0 &&
+      fileWithoutHeader.length <= options.maxLines
+    )
+    if(!isContentLengthAccepted) {
+      return {
+        error: error.FILE_LENGTH_ERROR_MESSAGE,
+        valid: false
+      }
+    }
+    return { valid: true }
+  }
+}
+
+(async () => {
+  const result = await File.csvToJson('./../mocks/threeItems-valid.csv')
+  console.log('result', result)
+})();
