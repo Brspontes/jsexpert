6e55e75209b6327d3258cc2baeb22ee096052b6e
Brian Pontes <brian.robert16@hotmail.com>
2022-06-21 21:53:14 -0300

    refatorando projeto

--- javascript-fundamentals/src/index.js
+++ javascript-fundamentals/src/index.js
@@ -1,34 +1,26 @@
-import DraftLog from "draftlog";
-import chalk from "chalk";
-import chalkTable from "chalk-table";
-import readline from "readline";
 import database from "./../database.json";
-import Person from "./person.js";
+import TerminalController from "./terminalController.js";
 
-DraftLog(console).addLineListener(process.stdin);
 const DEFAULT_LANG = "pt-BR";
-const options = {
-  leftPad: 2,
-  columns: [
-    { field: "id", name: chalk.cyan("id") },
-    { field: "vehicles", name: chalk.magenta("Vehicles") },
-    { field: "kmTraveled", name: chalk.blue("Km Traveled") },
-    { field: "from", name: chalk.red("From") },
-    { field: "to", name: chalk.green("To") },
-  ],
-};
+const STOP_TERM = ":q";
 
-const table = chalkTable(
-  options,
-  database.map((item) => new Person(item).formatted(DEFAULT_LANG))
-);
-const print = console.draft(table);
+const terminaController = new TerminalController();
+terminaController.initializeTerminal(database, DEFAULT_LANG);
 
-const terminal = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout,
-});
+async function mainLoop() {
+  try {
+    const answer = await terminaController.question("What??");
+    if (answer === STOP_TERM) {
+      terminaController.closeTerminal();
+      console.log("process finished");
+      return;
+    }
+    const person = Person.generateInstanceFromString(answer);
+    return mainLoop();
+  } catch (error) {
+    console.error("Deu ruim!!");
+    return mainLoop();
+  }
+}
 
-terminal.question("Qual o seu nome?", (msg) => {
-  console.log("msg: ", msg.toString());
-});
+await mainLoop();
