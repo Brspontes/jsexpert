8fe741f690c64358d9bb260bbbb51c8ac279e46f
Brian Pontes <brian.robert16@hotmail.com>
2022-07-07 20:37:07 -0300

    trabalhando com regex

---
+++ javascript-regex/project/app/src/textProcessorFluentApi.js
@@ -0,0 +1,20 @@
+class TextProcessorFluentAPI {
+  #content;
+  constructor(content) {
+    this.#content = content;
+  }
+
+  extractPeopleData() {
+    const matchPerson =
+      /(?<=[contratante|contratado]:\s{1})(?!\s)(.*\n.*?)$/gim;
+    const onlyPerson = this.#content.match(matchPerson);
+    this.#content = onlyPerson;
+    return this;
+  }
+
+  build() {
+    return this.#content;
+  }
+}
+
+module.exports = TextProcessorFluentAPI;
